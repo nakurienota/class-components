4050aefc3a3d5d7895b0e885ee12c76af019b028
stenyukovs <s.stenyukov@mixvel.com>
2026-05-04 23:22:16 +0400

    deploy:

--- class-components/vite.config.ts
+++ class-components/vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://rolling-scopes-school.github.io/nakurienota-REACT2026Q2/',
 });
